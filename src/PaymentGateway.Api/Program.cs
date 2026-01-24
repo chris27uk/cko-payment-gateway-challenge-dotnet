83965080087c4c1d331b30efc1330ef881b2395c
Chris Green <chris@openemail.org>
2026-01-24 14:34:05 +0000

    Fix DI and add http script.

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 using Microsoft.AspNetCore.Mvc;
 
+using PaymentGateway.Api.Features.GetPayment;
 using PaymentGateway.Api.Features.PostPayment;
 using PaymentGateway.Api.Features.PostPayment.Acquiring;
 using PaymentGateway.Api.Infrastructure;
@@ -9,7 +12,11 @@ using PaymentGateway.Api.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,8 +27,15 @@ builder.Services.Configure<ApiBehaviorOptions>(options => {
 
 builder.Services
     .AddSingleton<IAcquiringBankGateway>(sp => new ResilientAcquiringBankGateway(new HttpAcquiringBankGateway(sp.GetRequiredService<HttpClient>())))
-    .AddHttpClient<IAcquiringBankGateway>();
+    .AddHttpClient<IAcquiringBankGateway, HttpAcquiringBankGateway>(c => c.BaseAddress = new Uri("http://localhost:8080"))
+    .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+    {
+        // Force connection refresh every 2 minutes to respect DNS changes
+        PooledConnectionLifetime = TimeSpan.FromMinutes(2) 
+    })
+    .SetHandlerLifetime(Timeout.InfiniteTimeSpan);
 builder.Services.AddSingleton<IPostPaymentHandler, PostPaymentHandler>();
+builder.Services.AddSingleton<IGetPaymentHandler, GetPaymentHandler>();
 builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 builder.Services.AddSingleton<IObservabilityProbe, FakeObservabilityProbe>();
 builder.Services.AddSingleton<IPaymentsRepository>(_ => new ResilientPaymentsRepository(new FakePaymentsRepository(false, false, [])));
